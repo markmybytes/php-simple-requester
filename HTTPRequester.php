83050537e40bc35bc8c3333633d3419b275acaa9
markmybytes <markmybytes@gmail.com>
2025-02-13 17:11:07 +0800

    add methods to check  status code

--- HTTPRequester.php
+++ HTTPRequester.php
@@ -2,7 +2,7 @@
 
 namespace HTTPRequester;
 
-class HTTPRequester{
+class HTTPRequester {
 
 	// ----- HTTP Method Constants -----
 	const GET = "GET";
@@ -21,9 +21,12 @@ class HTTPRequester{
 	protected $parameters;
 	protected $outHeaders;
 	protected $payload;
-	private $curl;
 
-	public static function init(string $url){
+	private $curl;
+	private $requested = false;
+	private $frozen = false;
+
+	public static function init(string $url) {
 		return new HTTPRequester($url);
 	}
 
@@ -45,7 +48,7 @@ class HTTPRequester{
 		});
 	}
 
-	public function __destruct(){
+	public function __destruct() {
 		curl_close($this->curl);
 	}
 	
@@ -59,7 +62,7 @@ class HTTPRequester{
 	 * 
 	 * @param array $parameters query parameter(s) `['key' => value]`
 	 */
-	public function setQuery(array $parameters) {
+	public function withQuery(array $parameters) {
 		return $this->setOption(CURLOPT_URL, $this->url."?".http_build_query($parameters));
 	}
 
@@ -68,7 +71,7 @@ class HTTPRequester{
 	 * 
 	 * @param array $header
 	 */
-	public function setHeader(array $header){
+	public function setHeaders(array $header){
 		$this->outHeaders = $header;
 		$this->setOption(CURLOPT_HTTPHEADER, $header);
 		return $this;
@@ -80,7 +83,7 @@ class HTTPRequester{
 	 * 
 	 * @param array $payload
 	 */
-	public function setJsonPayload(array $payload) {
+	public function withJson(array $payload) {
 		$this->payload = $payload;
 		$this->setOption(CURLOPT_POSTFIELDS, json_encode($payload));
 		return $this;
@@ -92,7 +95,7 @@ class HTTPRequester{
 	 * 
 	 * @param array $payload
 	 */
-	public function setUrlencodePayload(array $payload) {
+	public function withForm(array $payload) {
 		$this->payload = $payload;
 		$this->setOption(CURLOPT_POSTFIELDS, http_build_query($payload));
 		return $this;
@@ -101,7 +104,7 @@ class HTTPRequester{
 	/**
 	 * set request payload/body with no encoding
 	 */
-	public function setRawPayload($payload) {
+	public function withRawPayload($payload) {
 		$this->payload = $payload;
 		$this->setOption(CURLOPT_POSTFIELDS, $payload);
 		return $this;
@@ -114,9 +117,9 @@ class HTTPRequester{
 	 * @param array $options curl option
 	 * @throws \RuntimeException indicate failure of setting the option
 	 */
-	public function setOption(int $option, $value){
+	public function setOption(int $option, $value) {
 		if (curl_setopt($this->curl, $option, $value) === false){
-			throw new \RuntimeException('Failed to set option');
+			throw new \RuntimeException('failed to set option');
 		}
 		return $this;
 	}
@@ -128,10 +131,19 @@ class HTTPRequester{
 	 * @param array $options curl options
 	 * @throws \RuntimeException indicate failure of setting options
 	 */
-	public function setOptions(array $options){
+	public function setOptions(array $options) {
 		if (curl_setopt_array($this->curl, $options) === false){
-			throw new \RuntimeException('Failed to set option');
+			throw new \RuntimeException('failed to set option');
 		}
+		return $this;
+	}
+
+	/**
+	 * freeze the instance after making the first request
+	 * (cannot make further request)
+	 */
+	public function frozen() {
+		$this->frozen = true;
 		return $this;
 	}
 
@@ -139,51 +151,56 @@ class HTTPRequester{
 	//					Request
 	// ----------------------------------------
 
-	public function request(string $method){
-		if (!isset($this->curl)){
-			throw new \BadMethodCallException("Please call HTTPRequester::init() first");
+	public function request(string $method) {
+		if ($this->requested && $this->frozen) {
+			throw new \BadMethodCallException("frozen, 
+				cannot use current instance to make request again.");
 		}
 		else if (!isset($this->curl)){
-			throw new \BadMethodCallException("Request URL is not set");
+			throw new \BadMethodCallException("please call HTTPRequester::init() first");
+		}
+		else if (!isset($this->curl)){
+			throw new \BadMethodCallException("request URL is not set");
 		}
 
 		curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, $method);
 		$this->response = curl_exec($this->curl);
+		$this->requested = true;
 		return $this;
 	}
 
 	/**
 	 * make a HTTP GET requset  
 	 */
-	public function get(){
+	public function get() {
 		return $this->request(self::GET);
 	}
 
 	/**
 	 * make a HTTP POST requset
 	 */
-	public function post(){
+	public function post() {
 		return $this->request(self::POST);
 	}
 
 	/**
 	 * make a HTTP PUT requset
 	 */
-	public function put(){
+	public function put() {
 		return $this->request(self::PUT);
 	}
 	
 	/**
 	 * make a HTTP DELETE requset  
 	 */
-	public function delete(){
+	public function delete() {
 		return $this->request(self::DELETE);
 	}
 
 	/**
 	 * make a HTTP HEAD requset  
 	 */
-	public function head(){
+	public function head() {
 		return $this->request(self::HEAD);
 	}
 
@@ -196,20 +213,29 @@ class HTTPRequester{
 	 * 
 	 * @return string|null
 	 */
-	public function response(){
+	public function response() {
 		return $this->response ?? null;
 	}
 
 	/**
-	 * decodes the response content using `json_decode`
+	 * decodes the json response
 	 *
 	 * @param bool|null $associative When `true` , JSON objects will be returned as associative `array`s; when `false` , JSON objects will be returned as `object`s. When `null` , JSON objects will be returned as associative `array`s or `object`s depending on whether `JSON_OBJECT_AS_ARRAY` is set in the `flags`.
 	 * @param int|null $depth Maximum nesting depth of the structure being decoded. The value must be greater than `0`, and less than or equal to `2147483647`.
 	 * @param int|null $flags Bitmask of `JSON_BIGINT_AS_STRING` , `JSON_INVALID_UTF8_IGNORE` , `JSON_INVALID_UTF8_SUBSTITUTE` , `JSON_OBJECT_AS_ARRAY` , `JSON_THROW_ON_ERROR` . The behaviour of these constants is described on the JSON constants page.
 	 * @return mixed Returns the value encoded in `json` in appropriate PHP type. Values `true`, `false` and `null` are returned as `true` , `false` and `null` respectively. `null` is returned if the `json` cannot be decoded or if the encoded data is deeper than the nesting limit.
 	 */
-	public function jsonResponse($associative = null, $depth = 512, $flags = 0){
-		return json_decode($this->response(), $associative, $depth, $flags);
+	public function jsonResponse($associative = null, $depth = 512, $flags = 0) {
+		return json_decode($this->response() ?? [], $associative, $depth, $flags);
+	}
+
+	/**
+	 * decodes the form response
+	 */
+	public function formResponse() {
+		$arr = [];
+		parse_str($this->response() ?? "", $arr);
+		return $arr;
 	}
 
 	/**
@@ -234,6 +260,27 @@ class HTTPRequester{
 	 */
 	public function statusCode() {
 		return $this->requestInfo(CURLINFO_HTTP_CODE);
+	}
+
+	/**
+	 * @return bool `true` if the status code is 2xx
+	 */
+	public function success() {
+		return 200 <= $this->statusCode() || $this->statusCode() < 300;
+	}
+
+	/**
+	 * @return bool `true` if the status code is 4xx
+	 */
+	public function clientError() {
+		return 400 <= $this->statusCode() || $this->statusCode() < 500;
+	}
+
+	/**
+	 * @return bool `true` if the status code is 5xx
+	 */
+	public function serverError() {
+		return 500 <= $this->statusCode() || $this->statusCode() < 600;
 	}
 
 	/**
